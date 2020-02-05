eda003325b529d70412d3941ee0f0d76b27a29e8
kumarnitesh2000 <46937660+kumarnitesh2000@users.noreply.github.com>
2020-02-05 09:07:55 -0700

    request

--- video_service.py
+++ video_service.py
@@ -22,13 +22,12 @@ def unique_id():
   }
 
 f=open("vid4.mp4","rb")
-print(len(f.read()))
+#print(len(f.read()))
 encode_byte=base64.b64encode(f.read())
 vid_file=encode_byte.decode("utf-8")
 
-is_Video=False
-if len(f.read())>10:
-  is_Video=True
+is_Video=True
+
 hour_minute_sec_mili=unique_id()['time_Id']
 date_month_year=unique_id()['date_Id']
 data={
